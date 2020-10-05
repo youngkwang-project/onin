561ad4c626a2eab74173765655823a619244e89f
youngkwang-git <sykkkkk412@naver.com>
2020-10-05 09:38:25 +0900

    memberDTO

---
+++ OnInProject/src/main/java/com/onin/project/dto/memberDTO.java
@@ -0,0 +1,17 @@
+package com.onin.project.dto;
+
+import lombok.Data;
+
+@Data
+public class memberDTO {
+	
+	int mno;
+	String email;
+	String pwd;
+	String name;
+	int tel;
+	String pro;
+	String alram;
+
+	
+}
