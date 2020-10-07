5db92ccd4e2307a8c7524d239343d58b8a99aa2b
youngkwang-git <sykkkkk412@naver.com>
2020-10-07 16:21:12 +0900

    DTO

---
+++ OnInProject/src/main/java/com/onin/project/dto/CategoryDTO.java
@@ -0,0 +1,17 @@
+package com.onin.project.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryDTO {
+
+	int cno;
+	String ctg;
+	String ctg_eng;
+	int dcno;
+	String dctg;
+	String dctg_eng;
+	String que;
+
+	
+}
