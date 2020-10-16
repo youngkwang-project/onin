a7d3282eff7c92839c1522a747c4b16cf857aef4
youngkwang-git <sykkkkk412@naver.com>
2020-10-16 18:01:18 +0900

    1

--- OnInProject/src/main/java/com/onin/project/mapper/EbMapper.java
+++ OnInProject/src/main/java/com/onin/project/mapper/EbMapper.java
@@ -9,26 +9,15 @@ import com.onin.project.dto.MemberDTO;
 
 
 public interface EbMapper {
-	//login
-	public MemberDTO memberLogin(MemberDTO memberdto);
-
-	//pwd 비밀번호찾기
-	public MemberDTO pwdForgot(MemberDTO memberdto);
 	
-	//임시비밀번호
-	public int updatepwd(MemberDTO memberdto);
 	
-	//카테고리 호출
-	public List<CategoryDTO> category();
-
-	//form값 db 저장
-	public int estimateDone(InvoiceDTO invoicedto);
+	
 	
 	//로그인 사용자가 보낸 의뢰요청서
 //	public List<InvoiceDTO> invoice(int from_mno);
 	
 	//invoice, category Join
-	public List<InvoiceJoinDTO> invoice(int from_mno);
+
 	
 }
 
